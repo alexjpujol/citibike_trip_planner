b881b008554878ca72c5cc802631f2c8876cb690
Alex Pujol <alex.j.pujol@gmail.com>
2016-05-09 12:45:19 -0400

    pins now on map

--- public/script.js
+++ public/script.js
@@ -2,6 +2,12 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.7230, lng: -74.0006},
-        zoom: 13
+        zoom: 12
+    });
+    var bikeLayer = new google.maps.BicyclingLayer();
+    bikeLayer.setMap(map);
+    
+    map.data.loadGeoJson('data.json');
+    map.data.setStyle({
     });
 }
